0137e1e0f4d7767425240a7cf92aee47774517aa
András Ács <acsandras@gmail.com>
2019-11-18 13:19:41 +0100

    Fixing issue

--- mongoUdenORM.js
+++ mongoUdenORM.js
@@ -2,12 +2,12 @@
 const MongoClient = require('mongodb').MongoClient;
 const uri = require('./settings').mongo_uri;
 console.log(uri);
-//const uri = "mongodb+srv://datamatiker:CodeWarrior8@cluster0-8j9w1.mongodb.net/test?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("sample_training").collection("zips");
   // perform actions on the collection object
-  let result = collection.findOne({"city" : {$regex : ".*WA.*"}}).then(
+  let result = collection.findOne({"city" : {$regex : ".*SA.*"}}).then(
       (result) => {
           console.log(result);
     });
